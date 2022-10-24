238cfc4a1ed7b7dee0cc6188427769c74b878cce
Federico Baldini <federico.baldini7@gmail.com>
2022-10-24 12:35:51 +0200

    Update main.rs
    Test the initialization of the different chess board squares representation.

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,31 @@
-fn main() {}
+mod definitions;
+mod init;
+
+fn main() {
+  let mut definitions = definitions::Definitions::new();
+  
+  init::init(&mut definitions);
+
+  for square in 0..definitions.square_120_to_square_64().len() {
+    if square % 10 == 0 {
+      println!();
+    }
+    print!(
+      "{} ",
+      definitions.square_120_to_square_64()[square as usize]
+    );
+  }
+
+  println!();
+  println!();
+
+  for square in 0..definitions.square_64_to_square_120().len() {
+    if square % 8 == 0 {
+      println!();
+    }
+    print!(
+      "{} ",
+      definitions.square_64_to_square_120()[square as usize]
+    );
+  }
+}
