f50700bedb97778e83fd2322c23fb869a7a1518f
Federico Baldini <federico.baldini7@gmail.com>
2022-10-26 19:09:50 +0200

    Create bitboards.rs
    Added bitboard file to manage the bitboards.

---
+++ src/bitboards.rs
@@ -0,0 +1,25 @@
+use crate::definitions::*;
+use crate::file_rank_to_square_120;
+
+pub fn print_bit_board(definitions: &mut Definitions, bit_board: u64) {
+  let shift_me: u64 = 1u64;
+
+  let mut square_120: i32;
+  let mut square_64: i32;
+
+  println!();
+  for rank in ChessboardRanks::R1 as i32..=ChessboardRanks::R8 as i32 {
+    for file in ChessboardFiles::A as i32..=ChessboardFiles::H as i32 {
+      square_120 = file_rank_to_square_120!(file, rank);
+      square_64 = definitions.board_120_squares()[square_120 as usize];
+      // bitwise operations
+      if ((shift_me << square_64) & bit_board) != 0 {
+        print!(" X ");
+      } else {
+        print!(" - ");
+      }
+    }
+    println!();
+  }
+  println!();
+}
