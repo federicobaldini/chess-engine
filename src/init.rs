cd80294545999699c92984bcf443bf8c23b231c9
Federico Baldini <federico.baldini7@gmail.com>
2022-10-26 19:09:57 +0200

    Update init.rs

--- src/init.rs
+++ src/init.rs
@@ -5,14 +5,14 @@ fn init_squares(definitions: &mut Definitions) {
   let mut square_120: i32;
   let mut square_64: i32 = 0;
 
-  definitions.square_120_to_square_64()[0..BOARD_SQUARE_NUMBER].fill(65);
-  definitions.square_64_to_square_120()[0..64].fill(120);
+  definitions.board_120_squares()[0..BOARD_SQUARE_NUMBER].fill(65);
+  definitions.board_64_squares()[0..64].fill(120);
 
   for rank in ChessboardRanks::R1 as i32..=ChessboardRanks::R8 as i32 {
     for file in ChessboardFiles::A as i32..=ChessboardFiles::H as i32 {
       square_120 = file_rank_to_square_120!(file, rank);
-      definitions.square_64_to_square_120()[square_64 as usize] = square_120;
-      definitions.square_120_to_square_64()[square_120 as usize] = square_64;
+      definitions.board_64_squares()[square_64 as usize] = square_120;
+      definitions.board_120_squares()[square_120 as usize] = square_64;
       square_64 += 1;
     }
   }
