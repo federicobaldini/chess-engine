6d727c48442db66058c7fc83a861953fdc89ee0d
Federico Baldini <federico.baldini7@gmail.com>
2022-10-29 15:06:23 +0200

    Update bitboards.rs

--- src/bitboards.rs
+++ src/bitboards.rs
@@ -8,7 +8,7 @@ pub fn print_bit_board(definitions: &mut Definitions, bit_board: u64) {
   let mut square_64: i32;
 
   println!();
-  for rank in ChessboardRanks::R1 as i32..=ChessboardRanks::R8 as i32 {
+  for rank in (ChessboardRanks::R1 as i32..=ChessboardRanks::R8 as i32).rev() {
     for file in ChessboardFiles::A as i32..=ChessboardFiles::H as i32 {
       square_120 = file_rank_to_square_120!(file, rank);
       square_64 = definitions.board_120_squares()[square_120 as usize];
