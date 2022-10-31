fa38c2918a9d5234d8567083323c5886232de831
Federico Baldini <federico.baldini7@gmail.com>
2022-10-31 10:35:43 +0100

    Update main.rs
    Tested the new bitboards functions.

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,17 @@ fn main() {
 
   init::init(&mut definitions);
 
-  // bitwise operations ("<<" bit shift, "|=" is like "+=" )
   test_bit_board |= 1u64
     << definitions.board_120_squares_in_64_squares_notation()[(definitions::Squares::D2) as usize];
-  println!("D2 pawn added to the bitboard!\n");
+  test_bit_board |= 1u64
+    << definitions.board_120_squares_in_64_squares_notation()[(definitions::Squares::D3) as usize];
+  test_bit_board |= 1u64
+    << definitions.board_120_squares_in_64_squares_notation()[(definitions::Squares::D4) as usize];
+
   bitboards::print_bit_board(&mut definitions, test_bit_board);
 
-  // bitwise operations ("<<" bit shift, "|=" is like "+=" )
-  test_bit_board |= 1u64
-    << definitions.board_64_squares_in_120_squares_notation()[(definitions::Squares::G3) as usize];
-  println!("G3 pawn added to the bitboard!\n");
+  println!("Count: {}", bitboards::count_bits(test_bit_board));
+  println!("Index: {}", bitboards::pop_first_bit(&mut test_bit_board));
+
   bitboards::print_bit_board(&mut definitions, test_bit_board);
 }
