3dd0eb85260805bcc49f444d3c48608e51180e63
Federico Baldini <federico.baldini7@gmail.com>
2022-10-31 10:35:28 +0100

    Update bitboards.rs
    Added functions to count the bits inside a bitboard and to pop out the least significant bit from a bitboard.

--- src/bitboards.rs
+++ src/bitboards.rs
@@ -1,8 +1,52 @@
 use crate::definitions::*;
 use crate::file_rank_to_square_120;
 
+const BIT_TABLE: [i32; 64] = [
+  63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2, 51, 21, 43, 45, 10, 18,
+  47, 1, 54, 9, 57, 0, 35, 62, 31, 40, 4, 49, 5, 52, 26, 60, 6, 23, 44, 46, 27, 56, 16, 7, 39, 48,
+  24, 59, 14, 12, 55, 38, 28, 58, 20, 37, 17, 36, 8,
+];
+
 /**
- * The bit board printed disposition follow this structure (as we play with white):
+ * We get a 64 bit unsigned integer as it overflowed the 32 bit size.
+ * For this scope it's necessary just invert and add one from the 64 bit type.
+ * This is necessary because Rust do not allow to overflow a type size as C does.
+ */
+fn get_overflow_as_32_bit(number: u64) -> u64 {
+  if (number & 0x80000000) != 0 {
+    return (((number as i64 & 0xFFFFFFFF) + 1) * (-1)) as u64;
+  } else {
+    return number & 0xFFFFFFFF;
+  }
+}
+
+/**
+ * It takes the first bit starting at the least significant bit in a bitboard and
+ * return the index that this bit was set at, so we know which square that bit was set on.
+ * Then that bit is set to zero.
+ * References:
+ * https://www.chessprogramming.org/Looking_for_Magics;
+ * https://stackoverflow.com/questions/30680559/how-to-find-magic-bitboards
+ * I will explain this function line by line if I will use it later.
+ */
+pub fn pop_first_bit(bit_board: &mut u64) -> i32 {
+  let b: u64 = *bit_board ^ (*bit_board - 1);
+  let fold: u64 = ((b & 0xffffffff) ^ (b >> 32)) as u64;
+  *bit_board &= *bit_board - 1;
+  BIT_TABLE[(get_overflow_as_32_bit(fold * 0x783a9b23) >> 26) as usize]
+}
+
+pub fn count_bits(mut bit_board: u64) -> u64 {
+  let mut counter: u64 = 0;
+  while counter < bit_board {
+    bit_board &= bit_board - 1;
+    counter += 1;
+  }
+  counter
+}
+
+/**
+ * The bitboard printed disposition follow this structure (as we play with white):
  *
  *    A B C D E F G H
  * 8  - - - - - - - -
