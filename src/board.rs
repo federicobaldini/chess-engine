a202b4c9084913d24a969a4fc92654ec090ed18b
Federico Baldini <federico.baldini7@gmail.com>
2022-11-05 10:32:03 +0100

    Update board.rs
    Fixed the code and added the reset for the big, minor, major pieces and pawns.

--- src/board.rs
+++ src/board.rs
@@ -1,10 +1,17 @@
 use crate::definitions::*;
 
-pub fn reset_board(board: &mut Board) {
-  for index 0..BOARD_SQUARE_NUMBER {
-    board.pieces()[index as usize] = Squares::OffBoard;
+pub fn reset_board(definitions: &mut Definitions, board: &mut Board) {
+  for index in 0..BOARD_SQUARE_NUMBER {
+    board.pieces()[index as usize] = Squares::OffBoard as i32;
   }
-  for index 0..64 {
-    board.pieces()[definitions.board_120_squares_in_64_squares_notation()[index]] = Pieces::Empty;
+  for index in 0..64 {
+    board.pieces()[definitions.board_120_squares_in_64_squares_notation()[index] as usize] =
+      Pieces::Empty as i32;
+  }
+  for index in 0..3 {
+    board.big_pieces_number()[index] = 0;
+    board.major_pieces_number()[index] = 0;
+    board.minor_pieces_number()[index] = 0;
+    board.pawns()[index] = 0u64;
   }
 }
