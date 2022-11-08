4081a49887b18f7a7bbcd421d3311260a6857f0b
Federico Baldini <federico.baldini7@gmail.com>
2022-11-08 09:24:11 +0100

    Update board.rs
    Added parse_fen() function to parse a string in Forsyth–Edwards notation.

--- src/board.rs
+++ src/board.rs
@@ -20,3 +20,57 @@ pub fn reset_board(definitions: &mut Definitions, board: &mut Board) {
   *board.castel_permission() = 0;
   *board.position_key() = 0u64;
 }
+
+// fen = Forsyth–Edwards Notation
+pub fn parse_fen(definitions: &mut Definitions, board: &mut Board, fen: &str) {
+  let mut rank: ChessboardRanks = ChessboardRanks::R8;
+  let mut file: ChessboardFiles = ChessboardFiles::A;
+  let mut piece: Pieces = Pieces::Empty;
+  let mut count: i32;
+  let mut char_index: usize = 0;
+  let mut square_120: i32;
+  let mut square_64: i32;
+  let mut fen_char: char = fen.as_bytes()[0] as char;
+
+  reset_board(definitions, board);
+
+  while (rank as i32 >= ChessboardRanks::R1 as i32) && char_index < fen.len() /* fen_char != '\0' */ {
+    count = 1;
+    match fen_char {
+      'c' => piece = Pieces::Bp,
+      'r' => piece = Pieces::Br,
+      'n' => piece = Pieces::Bn,
+      'b' => piece = Pieces::Bb,
+      'k' => piece = Pieces::Bk,
+      'q' => piece = Pieces::Bq,
+      'P' => piece = Pieces::Wp,
+      'R' => piece = Pieces::Wr,
+      'N' => piece = Pieces::Wn,
+      'B' => piece = Pieces::Wb,
+      'K' => piece = Pieces::Wk,
+      'Q' => piece = Pieces::Wq,
+      '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' => {
+        piece = Pieces::Empty;
+        count = fen_char as i32 - '0' as i32
+      }
+      '/' | ' ' => {
+        rank = ChessboardRanks::from_u32(rank as u32 - 1);
+        file = ChessboardFiles::A;
+        char_index += 1;
+        // fen_char = fen.as_bytes()[char_index] as char;
+      }
+      _ => println!("FEN error"),
+    }
+
+    for _ in 0..count {
+      square_64 = rank as i32 * 8 + file as i32;
+      square_120 = definitions.board_64_squares_in_120_squares_notation()[square_64 as usize];
+      if piece as i32 != Pieces::Empty as i32 {
+        board.pieces()[square_120 as usize] = piece as i32;
+      }
+      file = ChessboardFiles::from_u32(file as u32 + 1);
+    }
+    char_index += 1;
+    fen_char = fen.as_bytes()[char_index] as char;
+  }
+}
