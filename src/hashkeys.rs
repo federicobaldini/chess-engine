1d67fc0c8bb62d3631a687e7e84ad034e62737e5
Federico Baldini <federico.baldini7@gmail.com>
2022-11-08 15:06:01 +0100

    Update hashkeys.rs
    Now the hashkey functions works with the copy of the structures and not their references (more safe).

--- src/hashkeys.rs
+++ src/hashkeys.rs
@@ -1,26 +1,29 @@
+use crate::board::*;
 use crate::definitions::*;
 
-fn generate_position_key(definitions: &mut Definitions, board: &mut Board) -> u64 {
+pub fn generate_position_key(definitions: Definitions, board: Board) -> u64 {
   let mut final_key: u64 = 0;
   let mut piece: Pieces;
 
   for square_120 in 0..BOARD_SQUARE_NUMBER {
-    piece = Pieces::from_u32(board.pieces()[square_120 as usize] as u32);
-    if piece as i32 != Squares::NoSquare as i32 && piece as i32 != Pieces::Empty as i32 {
-      final_key ^= definitions.piece_keys()[piece as usize][square_120 as usize];
+    if board.pieces()[square_120 as usize] as u32 != Squares::OffBoard as u32 {
+      piece = Pieces::from_u32(board.pieces()[square_120 as usize] as u32);
+      if piece as i32 != Squares::NoSquare as i32 && piece as i32 != Pieces::Empty as i32 {
+        final_key ^= definitions.piece_keys()[piece as usize][square_120 as usize];
+      }
     }
   }
 
-  if *board.side() as i32 == Colors::White as i32 {
-    final_key ^= *definitions.side_key();
+  if board.side() as i32 == Colors::White as i32 {
+    final_key ^= definitions.side_key();
   }
 
-  if *board.en_passant_square() as i32 != Squares::NoSquare as i32 {
+  if board.en_passant_square() as i32 != Squares::NoSquare as i32 {
     final_key ^=
-      definitions.piece_keys()[Pieces::Empty as usize][*board.en_passant_square() as usize];
+      definitions.piece_keys()[Pieces::Empty as usize][board.en_passant_square() as usize];
   }
 
-  final_key ^= definitions.castle_keys()[*board.castel_permission() as usize];
+  final_key ^= definitions.castle_keys()[board.castel_permission() as usize];
 
   final_key
 }
