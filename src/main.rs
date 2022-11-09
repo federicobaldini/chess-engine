6a5613983e62a3f0fc0ce6ad3a479db78151be35
Federico Baldini <federico.baldini7@gmail.com>
2022-11-09 09:59:19 +0100

    Update main.rs
    Tested the parse_fen() and print_board() functions.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,22 @@ mod bitboards;
 mod board;
 mod definitions;
 mod hashkeys;
-mod init;
-use rand::Rng;
+
+const FEN1: &str = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1";
+const FEN2: &str = "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2";
+const FEN3: &str = "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2";
 
 fn main() {
   let mut definitions = definitions::Definitions::new();
-  init::init(&mut definitions);
+  let mut board = board::Board::new();
+
+  definitions.init();
+  //board.parse_fen(definitions, definitions::START_FEN);
+  //board.print_board(definitions);
+  board.parse_fen(definitions, FEN1);
+  board.print_board(definitions);
+  board.parse_fen(definitions, FEN2);
+  board.print_board(definitions);
+  board.parse_fen(definitions, FEN3);
+  board.print_board(definitions);
 }
