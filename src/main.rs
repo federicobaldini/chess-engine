9cffa544fd7fae6f2848cbd7c32f3e68971b0dcc
Federico Baldini <federico.baldini7@gmail.com>
2023-01-26 22:02:02 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use crate::definitions::Colors;
+use board::Board;
+use definitions::Definitions;
 
 mod bitboards;
 mod board;
@@ -8,17 +9,10 @@ mod hashkeys;
 const TEST_FEN: &str = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
 
 fn main() {
-  let mut definitions = definitions::Definitions::new();
-  let mut board = board::Board::new();
-
+  let mut definitions: Definitions = definitions::Definitions::new();
   definitions.init();
-  board.parse_fen(&definitions, TEST_FEN);
-  board.print_board(&definitions);
+  let mut board: Board = board::Board::new(&definitions);
 
-  println!("\nWhite Pawns:");
-  bitboards::print_bitboard(&definitions, board.pawns()[Colors::White as usize]);
-  println!("\nBlack Pawns:");
-  bitboards::print_bitboard(&definitions, board.pawns()[Colors::Black as usize]);
-  println!("\nAll Pawns:");
-  bitboards::print_bitboard(&definitions, board.pawns()[Colors::Both as usize]);
+  board.parse_fen(TEST_FEN);
+  board.print_board();
 }
