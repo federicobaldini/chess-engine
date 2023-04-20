fb94f943bd7fead4cc8e6ffd30ebe5e33245f53e
Federico Baldini <federico.baldini7@gmail.com>
2023-04-20 09:29:54 +0200

    Update main.rs
    Tested the square_attacked function.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,28 @@
+use attack::show_squares_attacked_by_side;
 use board::Board;
-use definitions::Definitions;
+use definitions::{Colors, Definitions};
 
+mod attack;
 mod bitboards;
 mod board;
 mod definitions;
 mod hashkeys;
 
-const TEST_FEN: &str = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+const TEST_FEN_1: &str = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
+const TEST_FEN_2: &str = "8/3q1p2/8/5P2/4Q3/8/8/8 w - - 0 2";
 
 fn main() {
   let mut definitions: Definitions = definitions::Definitions::new();
   definitions.init();
   let mut board: Board = board::Board::new(&definitions);
 
-  board.parse_fen(TEST_FEN);
+  board.parse_fen(TEST_FEN_2);
+  // board.check_board();
   board.print_board();
+
+  println!("\n\nWhite attacking:");
+  show_squares_attacked_by_side(Colors::White, &board);
+
+  println!("\n\nBlack attacking:");
+  show_squares_attacked_by_side(Colors::Black, &board);
 }
