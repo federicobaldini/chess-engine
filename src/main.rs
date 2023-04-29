b6d9d082627ac96ac206e7823712533f65322f5b
Federico Baldini <federico.baldini7@gmail.com>
2023-04-29 10:50:46 +0200

    Update main.rs
    Tested the Move struct and its implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use attack::show_squares_attacked_by_side;
 use board::Board;
-use definitions::{Colors, Definitions};
+use definitions::{Definitions, Move, Pieces};
 
 mod attack;
 mod bitboards;
@@ -9,20 +8,59 @@ mod definitions;
 mod hashkeys;
 
 const TEST_FEN_1: &str = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
-const TEST_FEN_2: &str = "8/3q1p2/8/5P2/4Q3/8/8/8 w - - 0 2";
+
+fn print_binary(mov_e: i32) {
+  println!("As binary:");
+  for index in (0..=27).rev() {
+    if ((1 << index) & mov_e) != 0 {
+      print!("1");
+    } else {
+      print!("0");
+    }
+    if index != 28 && (index % 4) == 0 {
+      print!(" ");
+    }
+  }
+  println!();
+}
 
 fn main() {
   let mut definitions: Definitions = definitions::Definitions::new();
   definitions.init();
   let mut board: Board = board::Board::new(&definitions);
 
-  board.parse_fen(TEST_FEN_2);
+  board.parse_fen(TEST_FEN_1);
   // board.check_board();
   board.print_board();
 
-  println!("\n\nWhite attacking:");
-  show_squares_attacked_by_side(Colors::White, &board);
+  let from: i32 = 6;
+  let to: i32 = 12;
+  let captured: Pieces = Pieces::Wr;
+  let promoted: Pieces = Pieces::Br;
+  let mut temp_move: Move = Move::new(
+    (from) | (to << 7) | ((captured as i32) << 14) | ((promoted as i32) << 20),
+    0,
+  );
 
-  println!("\n\nBlack attacking:");
-  show_squares_attacked_by_side(Colors::Black, &board);
+  println!("\ndec: {} hex: {:x}", temp_move.mov_e(), temp_move.mov_e());
+  print_binary(temp_move.mov_e());
+
+  println!(
+    "\nfrom: {} to: {} captured: {} promoted: {}",
+    temp_move.from_square(),
+    temp_move.to_square(),
+    temp_move.captured_piece(),
+    temp_move.promoted()
+  );
+
+  // temp_move.set_mov_e(temp_move.mov_e() | 0x80000);
+
+  println!(
+    "\nIs a pawn start: {}",
+    if temp_move.pawn_start() != 0 {
+      "YES"
+    } else {
+      "NO"
+    }
+  );
 }
